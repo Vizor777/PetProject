2b4f54a5ba3ae55bf3b1dc19cae7e9e40b338143
Ivan <pipkamax1993@gmail.com>
2020-12-03 22:51:06 +0300

    added functions

--- script.js
+++ script.js
@@ -21,22 +21,76 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+// Задание №3
+/* Задание на урок:
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+"use strict";
+
+let numberOfFilms;
+
+function showMyDB(privat) {
+  if (!privat) {
+    console.log(personalMovieDB);
+  }
+}
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, "");
+  }
+}
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let lastOfFilms = prompt("Один из последних просмотренных фильмов?", ""),
+        ratingOfFilms = prompt("На сколько оцените его?", "");
+    if (lastOfFilms != null && ratingOfFilms != null && lastOfFilms != '' && ratingOfFilms != '' && lastOfFilms.length < 50 && ratingOfFilms.length < 50 ) {
+      personalMovieDB.movies[lastOfFilms] = [ratingOfFilms];
+    } else {
+      i--;
+    }
+  }
+}
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотренно давольно мало фильмов");
+  } else if (10 <= personalMovieDB.count <= 30) {
+    console.log("Вы класстческий зритель");
+  } else if (personalMovieDB.count > 30) {
+    console.log("Вы - киноман");
+  } else {
+    console.log("Ошибка");
+  }
+}
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
 
 let personalMovieDB = {
   count: numberOfFilms,
-  movies: new Object(),
-  actors: new Object(),
-  genres: new Array(),
+  movies: {},
+  actors: {},
+  genres: [],
   privat: false
-}
+};
 
-for (let i = 0; i < 2; i++) {
-  const lastOfFilms = prompt("Один из последних просмотренных фильмов?", "");
-  const ratingOfFilms = prompt("На сколько оцените его?", ""); 
-  personalMovieDB.movies[lastOfFilms] = [ratingOfFilms];
-}
+rememberMyFilms();
+detectPersonalLevel();
 
 console.log(personalMovieDB);
+
+
